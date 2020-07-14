cf336d21e1472c35aa2cb205751d57dec2e18a3b
Diego <diegomf.mendoza@gmail.com>
2020-07-14 14:55:51 -0500

    commit ejemplo

--- app/Http/Controllers/AprobacionController.php
+++ app/Http/Controllers/AprobacionController.php
@@ -173,7 +173,7 @@ class AprobacionController extends Controller
         // $query2ejm=;
         // dd($query);
         $data=DB::connection('sqlsrv')
-                ->select('EXEC getrecord_Returndocumento '.DB::raw("'001','EAR'"),[]);
+                ->select('SET NOCOUNT ON; EXEC getrecord_Returndocumento '.DB::raw("'001','EAR'"),[]);
         dd($data);
         return response()->json($this->keyMin($data));
     }
