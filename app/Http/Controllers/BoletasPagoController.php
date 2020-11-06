0b4687bacd3c89a2a44ffba0c6d0dc19cc9e3a15
Diego Mendoza <diegomf.mendoza@gmail.com>
2020-11-06 17:51:08 -0500

    datos

--- app/Http/Controllers/BoletasPagoController.php
+++ app/Http/Controllers/BoletasPagoController.php
@@ -11,98 +11,112 @@ class BoletasPagoController extends Controller
 {
     public function index(Request $request){
         $codigo=$request->codigo;
+
         $lista=DB::connection('sqlsrv')
-                ->select(DB::raw("SET DATEFIRST 1;
-                SELECT	MP.TIPO tipo,
-                                -- MP.PERIODO periodo,
-                                MP.IDPLANILLA idplanilla,
-                                MAX(MP.SEMANA),
-                                MAX(CASE WHEN PL.TIPO_ENVIO = 'S'  THEN DATEPART(ISO_WEEK,PP.FECHA_INI) ELSE MP.SEMANA END) semana,
-                                PL.TIPO_ENVIO envio,
-                                SUM(M.importemof) monto,
-                                -- GROUP_CONCAT_S(MP.IDMOVIMIENTO),
-                                 --STUFF((	SELECT ', ' + MP2.IDMOVIMIENTO AS [text()]
-                                        -- 	FROM movimiento_planilla MP2
-                                        -- 	WHERE MP2.IDMOVIMIENTO=MP.IDMOVIMIENTO
-                                        -- 	FOR XML PATH('')), 1, 2, '')
-                                  -- movimiento,
-                                SUBSTRING(MP.PERIODO, 1, 4) anio,
-                                DATEPART(ISO_WEEK,PP.FECHA_INI) semana_real
-                                -- PP.FECHA_INI
+                ->select(DB::raw("SET DATEFIRST 1;".
+                "SELECT	MP.TIPO tipo,
+                        -- MP.PERIODO periodo,
+                        MP.IDPLANILLA idplanilla,
+                        MIN(MP.PERIODO) min_periodo,
+                                        MIN(MP.SEMANA) min_semana,
+                                        MAX(MP.PERIODO) max_periodo,
+                                        MAX(MP.SEMANA) max_semana,
+                                        SUBSTRING(MP.PERIODO, 1, 4) anio,
+                        MAX(CASE WHEN PL.TIPO_ENVIO = 'S'  THEN DATEPART(ISO_WEEK,PP.FECHA_INI) ELSE MP.SEMANA END) semana,
+                        PL.TIPO_ENVIO envio,
+                                        CONCAT(MAX(MP.IDMOVIMIENTO),',',MIN(MP.IDMOVIMIENTO)) movimientos,
+                                        --MIN(MP.IDMOVIMIENTO) idmovimiento_min,
+                        SUM(M.importemof) monto
                 from movimiento_planilla MP
                 INNER JOIN PLANILLA PL ON PL.IDPLANILLA=MP.IDPLANILLA
                 INNER JOIN PERIODO_PLANILLA PP ON PP.PERIODO=MP.PERIODO AND PP.SEMANA=MP.SEMANA AND MP.IDPLANILLA=PP.IDPLANILLA
                 INNER JOIN COBRARPAGARDOC C ON MP.IDMOVIMIENTO=C.idmovplanilla
                 INNER JOIN MOVCTACTE M ON M.IDEMPRESA=C.IDEMPRESA AND M.IDREFERENCIA=C.IDCOBRARPAGARDOC
-                where IDCODIGOGENERAL = '77382978'
+                where IDCODIGOGENERAL = ?
                 AND MP.TIPO='N'
                 AND M.tabla='INGRESOEGRESOCABA' 
                 AND M.factor=-1
-                GROUP BY MP.TIPO, MP.IDPLANILLA, PL.TIPO_ENVIO, SUBSTRING(MP.PERIODO, 1, 4), DATEPART(ISO_WEEK,PP.FECHA_INI)"),
+                GROUP BY MP.TIPO, MP.IDPLANILLA, PL.TIPO_ENVIO, SUBSTRING(MP.PERIODO, 1, 4), DATEPART(ISO_WEEK,PP.FECHA_INI)
+                ORDER BY min_periodo DESC, min_semana DESC"),
                 [$codigo]);
         return response()->json($lista);
     }
     public function show(Request $request){
         $codigo=$request->codigo;
-
+        $sCodigo="";
+        $arrayCodigos=explode(',',$codigo);
+        if (count($arrayCodigos)==1) {
+                $sCodigo="?";        
+        }else{
+                $sCodigo="?,?";        
+        }
         //sueldo
         $sueldo=DB::connection('sqlsrv')
-                ->select("select D.CALCULO, D.VALOR, C.DESCR_CORTA, C.IDTIPOCONCEPTO , C.ORDEN from deta_movimiento_planilla D
+                ->select("SELECT D.CALCULO, D.VALOR, C.DESCR_CORTA, C.IDTIPOCONCEPTO , C.ORDEN from deta_movimiento_planilla D
                 INNER JOIN CONCEPTOS C ON C.IDCONCEPTO = D.IDCONCEPTO
-                WHERE idmovimiento = ?
+                WHERE idmovimiento in ($sCodigo)
                 AND C.DESCR_CORTA='BASICO'
                 AND IDTIPOCONCEPTO='IN'",
-                [$codigo])[0]->VALOR;
-        // dd($request->all(),$sueldo);
+                $arrayCodigos)[0]->VALOR;
 
         //REMUNERACIONES
         $ingresos=DB::connection('sqlsrv')
-                ->select("select D.CALCULO, D.VALOR, C.DESCR_CORTA, C.IDTIPOCONCEPTO , C.ORDEN 
+                ->select("SELECT SUM(D.CALCULO) CALCULO, C.DESCR_CORTA, C.IDTIPOCONCEPTO 
                 from deta_movimiento_planilla D
                 INNER JOIN CONCEPTOS C ON C.IDCONCEPTO = D.IDCONCEPTO
-                WHERE idmovimiento = ?
-                AND IDTIPOCONCEPTO='IN'",
-                [$codigo]);
+                WHERE idmovimiento IN ($sCodigo)
+                AND IDTIPOCONCEPTO='IN'
+                GROUP BY C.DESCR_CORTA, C.IDTIPOCONCEPTO",
+                $arrayCodigos);
         //DESCUENTOS
         $descuentos=DB::connection('sqlsrv')
-                ->select("select D.CALCULO, D.VALOR, C.DESCR_CORTA, C.IDTIPOCONCEPTO , C.ORDEN from deta_movimiento_planilla D
+                ->select("SELECT SUM(D.CALCULO) CALCULO, C.DESCR_CORTA, C.IDTIPOCONCEPTO from deta_movimiento_planilla D
                 INNER JOIN CONCEPTOS C ON C.IDCONCEPTO = D.IDCONCEPTO
-                WHERE idmovimiento = ?
-                AND IDTIPOCONCEPTO='DE'",
-                [$codigo]);
+                WHERE idmovimiento IN ($sCodigo)
+                AND IDTIPOCONCEPTO='DE'
+                GROUP BY C.DESCR_CORTA, C.IDTIPOCONCEPTO",
+                $arrayCodigos);
         //SEGURO
         $seguro=DB::connection('sqlsrv')
-                ->select("select D.CALCULO, D.VALOR, C.DESCR_CORTA, C.IDTIPOCONCEPTO , C.ORDEN from deta_movimiento_planilla D
+                ->select("SELECT SUM(D.CALCULO) CALCULO, C.DESCR_CORTA, C.IDTIPOCONCEPTO from deta_movimiento_planilla D
                 INNER JOIN CONCEPTOS C ON C.IDCONCEPTO = D.IDCONCEPTO
-                WHERE idmovimiento = ?
-                AND IDTIPOCONCEPTO='AE'",
-                [$codigo]);
+                WHERE idmovimiento IN ($sCodigo)
+                AND IDTIPOCONCEPTO='AE'
+                GROUP BY C.DESCR_CORTA, C.IDTIPOCONCEPTO",
+                $arrayCodigos);
         //tiempos
         $tiempos=DB::connection('sqlsrv')
-                ->select("select D.CALCULO, D.VALOR, C.DESCR_CORTA, C.IDTIPOCONCEPTO , C.ORDEN from deta_movimiento_planilla D
+                ->select("SELECT SUM(D.CALCULO) CALCULO, C.DESCR_CORTA, C.IDTIPOCONCEPTO from deta_movimiento_planilla D
                 INNER JOIN CONCEPTOS C ON C.IDCONCEPTO = D.IDCONCEPTO
-                WHERE idmovimiento = ?
-                AND IDTIPOCONCEPTO='TI'",
-                [$codigo]);
+                WHERE idmovimiento IN ($sCodigo)
+                AND IDTIPOCONCEPTO='TI'
+                GROUP BY C.DESCR_CORTA, C.IDTIPOCONCEPTO",
+                $arrayCodigos);
         //totales
         $totales=DB::connection('sqlsrv')
-                ->select("select D.CALCULO, D.VALOR, C.DESCR_CORTA, C.IDTIPOCONCEPTO , C.ORDEN from deta_movimiento_planilla D
+                ->select("SELECT SUM(D.CALCULO) CALCULO, C.DESCR_CORTA, C.IDTIPOCONCEPTO from deta_movimiento_planilla D
                 INNER JOIN CONCEPTOS C ON C.IDCONCEPTO = D.IDCONCEPTO
-                WHERE idmovimiento = ?
-                AND IDTIPOCONCEPTO='TO'",
-                [$codigo]);
+                WHERE idmovimiento IN ($sCodigo)
+                AND IDTIPOCONCEPTO='TO'
+                GROUP BY C.DESCR_CORTA, C.IDTIPOCONCEPTO",
+                $arrayCodigos);
 
         $periodo=DB::connection('sqlsrv')
-                ->select("SELECT 
-                                PP.PERIODO, 
-                                PP.SEMANA, 
-                                FORMAT(PP.FECHA_INI,'dd/MM/yyyy ') FECHA_INI, 
-                                FORMAT(PP.FECHA_FIN,'dd/MM/yyyy ') FECHA_FIN 
+                ->select("SET DATEFIRST 1;".
+                        "SELECT 
+                                MP.IDCODIGOGENERAL,
+                                RTRIM(PL.TIPO_ENVIO) ENVIO,
+                                SUBSTRING(MP.PERIODO, 1, 4) anio,
+                                MAX(CASE WHEN PL.TIPO_ENVIO = 'S'  THEN DATEPART(ISO_WEEK,PP.FECHA_INI) ELSE MP.SEMANA END) semana,
+                                FORMAT(MIN(PP.FECHA_INI),'dd/MM/yyyy') FECHA_INI, 
+                                FORMAT(MAX(PP.FECHA_FIN),'dd/MM/yyyy') FECHA_FIN 
                         FROM MOVIMIENTO_PLANILLA MP
+                        INNER JOIN PLANILLA PL ON PL.IDPLANILLA=MP.IDPLANILLA
                         INNER JOIN PERIODO_PLANILLA PP ON PP.PERIODO=MP.PERIODO AND PP.SEMANA = MP.SEMANA
-                        where idmovimiento = ?",
-                        [$codigo])[0];
-        
+                        where idmovimiento IN ($sCodigo)
+                        GROUP BY MP.IDCODIGOGENERAL,PL.TIPO_ENVIO,SUBSTRING(MP.PERIODO, 1, 4)  ",
+                        $arrayCodigos)[0];
+        dd($periodo);
 
         $lista=[
             "sueldo"=> $sueldo,
